fe4520ee53fcab2e5434977bcf359fe39dd3147d
Daria Tsvetkova <lorminel22@gmail.com>
2024-03-19 17:40:03 +0300

    Updated variables in Main.java

--- src/main/java/com/lorminel22/Main.java
+++ src/main/java/com/lorminel22/Main.java
@@ -3,51 +3,51 @@ package com.lorminel22;
 public class Main {
     public static void main(String[] args) {
 
-        byte first = 100;
-        byte second = 40;
-       // byte sum = first + second;    ошибка
-        System.out.println(first + second); // сумма вычисляется
-        System.out.println(first * second);
+        byte b1 = 100;
+        byte b2 = 40;
+       // byte sum = b1 + b2;    ошибка
+        System.out.println(b1 + b2); // сумма вычисляется
+        System.out.println(b1 * b2);
 
-        int firstByteToInt = first;
-        int secondByteToInt = second;
-        int sum = firstByteToInt + secondByteToInt;
+        int ByteToInt1 = b1;
+        int ByteToInt2 = b2;
+        int sum = ByteToInt1 + ByteToInt2;
         System.out.println(sum);
 
-        short firstShort = 30000;
-        short secondShort = 5000;
-        short thirdShort = -15677;
+        short s1 = 30000;
+        short s2 = 5000;
+        short s3 = -15677;
 
-        short multipleShort = (short) (firstShort * secondShort);   //результат -11904
+        short multipleShort = (short) (s1 * s2);   //результат -11904
         System.out.println(multipleShort);
-        short div = (short) (firstShort / secondShort);
-        System.out.println(div);
-        System.out.println(thirdShort + firstShort*5);
+        short divShort = (short) (s1 / s2);
+        System.out.println(divShort);
+        System.out.println(s3 + s1 * 5);
 
-        boolean a = true;
-        boolean b = false;
-        System.out.println(!a);
-        System.out.println(a == b );
-        System.out.println(a && b );
-        System.out.println(a || b );
+        boolean aBool = true;
+        boolean bBool = false;
+        System.out.println(!aBool);
+        System.out.println(aBool == bBool);
+        System.out.println(aBool && bBool);
+        System.out.println(aBool || bBool);
 
         if(5 >= 6){
-            System.out.println(a);
+            System.out.println(aBool);
         } else {
-            System.out.println(b);
+            System.out.println(bBool);
         }
 
 
       // вычисления разных типов данных
-        double firstD = 0.001;
-        int firstInt = 40;
-        System.out.print((firstD + firstInt) + "\n" +
-                         (firstD - firstInt) + "\n" +
-                         (firstD * firstInt) + "\n" +
-                         (firstD / firstInt) + "\n");
+        double d1 = 0.001;
+        int i1 = 40;
+        System.out.print((d1 + i1) + "\n" +
+                         (d1 - i1) + "\n" +
+                         (d1 * i1) + "\n" +
+                         (d1 / i1) + "\n");
 
-        System.out.println(firstD % 10);
-        System.out.println(firstInt % 10);
+        System.out.println(d1 % 10);
+        System.out.println(i1 % 10);
 
     }
 }
